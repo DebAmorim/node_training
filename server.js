a98aa2182166fe611ff8970233149c1cfc45c613
Debora Amorim <debora.amorim@sonner.com.br>
2020-06-28 22:15:57 -0300

    Criando a primeira rota

--- server.js
+++ server.js
@@ -4,5 +4,14 @@ const express = require('express');
 //e views (não utilizadas aqui por ser uma API REST, mas são as formas de visualização)
 const app = express();
 
+app.get('/', (req, res) => {
+    //req: requisição feita ao servidor acessando essa url.
+    //Contém todos os parâmetros da requisição:
+    //parâmetros, corpo da requisição, cabeçalho, usuário, autenticação, ip
+
+    //res: resposta dada à requisição. Pode ser qualquer valor
+    res.send('Hello, Débora!')
+});
+
 //diz para a aplicação ouvir a porta 3001 do navegador
 app.listen(3001);
