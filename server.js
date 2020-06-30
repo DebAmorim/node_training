e15bfaa7e01006b562a5479ca8d7f1613514dfb2
Debora Amorim <debora.amorim@sonner.com.br>
2020-06-30 14:10:37 -0300

    Rotas de produtos

--- server.js
+++ server.js
@@ -8,15 +8,9 @@ const app = express();
 //Iniciando o DB
 mongoose.connect('mongodb://localhost:27017/nodeapi', {useNewUrlParser: true});
 requireDir("./src/models");
-// require sem a biblioteca require-dir 
-// require('./src/models/product');
-
-//pra testar se o model está funcionando:
-// const product = mongoose.model('Product');
 
 //Rotas
 app.use("/api", require("./src/routes"));
-
 
 app.listen(3001);
 
--- src/routes.js
+++ src/routes.js
@@ -1,14 +1,8 @@
 const express = require("express");
 const routes = express.Router();
 
-routes.get('/', (req, res) => {
-    // product.create({
-    //     title: 'React Native',
-    //     description: 'Build native apps with React',
-    //     url: 'http://github.com/facebook/react-native'
-    // });
-    
-    return res.send('Hello, Deb!')
-});
+const ProductController = require("./controllers/ProductController");
+
+routes.get('/products', ProductController.index);
 
 module.exports = routes;
