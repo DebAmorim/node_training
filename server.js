4a8c93a65f3e94ddd9cceb1c8c56d6adb71e29bb
Debora Amorim <debora.amorim@sonner.com.br>
2020-07-01 13:27:52 -0300

    rota post

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const requireDir = require('require-dir');
 
 //Iniciando o app
 const app = express();
+//permite que eu envie dados para a aplicação no formato de json
+app.use(express.json());
 
 //Iniciando o DB
 mongoose.connect('mongodb://localhost:27017/nodeapi', {useNewUrlParser: true});
--- src/controllers/ProductController.js
+++ src/controllers/ProductController.js
@@ -6,5 +6,11 @@ module.exports = {
     async index(req, res){
         const products = await Product.find();
         return res.json(products);
+    },
+
+    async store(req, res){
+        //Criação
+        const product = await Product.create(req.body);
+        return res.json(product);
     }
 };
