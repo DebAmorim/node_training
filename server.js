b47d71bc0133c4b0796141b1468f4331ee318170
Debora Amorim <debora.amorim@sonner.com.br>
2020-07-23 08:57:15 -0300

    Adicionando cors

--- server.js
+++ server.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const requireDir = require('require-dir');
+const cors = require('cors');
 
 //Iniciando o app
 const app = express();
 //permite que eu envie dados para a aplicação no formato de json
 app.use(express.json());
+//dessa forma libera o acesso a todos os domínios
+app.use(cors());
 
 //Iniciando o DB
 mongoose.connect('mongodb://localhost:27017/nodeapi', {useNewUrlParser: true});
